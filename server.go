19a63f77151589b5a0a7aed9bcaea79b89168c59
Calamitas <calamitas@pornhub.com>
2022-07-13 21:50:20 -0500

    added logging

--- server.go
+++ server.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ip := GetRealIP(r)
 	fmt.Fprintf(w, ip)
+	WritetoFile(ip)
 }
 
 func GetRealIP(r *http.Request) string {
@@ -23,6 +25,17 @@ func GetRealIP(r *http.Request) string {
 		IP = "No IPV4 address"
 	}
 	return IP
+}
+func WritetoFile(IP string) {
+	f, err := os.OpenFile("ListofIPs.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(IP + "\n"); err != nil {
+		log.Println(err)
+	}
 }
 func main() {
 	http.HandleFunc("/", handler)
